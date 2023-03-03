79962b086fc5ff35a7650172a1c06c77f2a9783d
Alex Constancio <aconstancio@livefront.com>
2023-03-03 09:35:47 -0600

    SwiftUI Templates

---
+++ SwiftUI-Pentimento-Templates/SwiftUI-Pentimento-Templates/SwiftUI_Pentimento_TemplatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Pentimento_TemplatesApp.swift
+//  SwiftUI-Pentimento-Templates
+//
+//  Created by Alex Constancio on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Pentimento_TemplatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
